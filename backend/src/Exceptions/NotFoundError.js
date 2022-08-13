a39612b48bb13c7a03c71ebc29318ad8b7faec80
agungardiyanta <agungardiyanta@gmail.com>
2022-08-13 13:20:20 +0700

    add error handling

--- backend/src/Exceptions/NotFoundError.js
+++ backend/src/Exceptions/NotFoundError.js
@@ -1,8 +1,25 @@
+const BadRequest = (res, message) =>{
+  res.status(400).json({
+    status: false,
+    message,
+  });
+};
+
 const NotFoundError = (res, message) => {
   res.status(404).json({
     status: false,
     message,
   });
 };
+
+
+const serverError = (res, message) => {
+  res.status(500).json({
+    status: false,
+    message,
+  })
+
+};
+
 
 module.exports = NotFoundError;
