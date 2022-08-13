66e6b9648bf566da3491d80d4b5973f44a620352
Gerry Steven <sgerrys93@gmail.com>
2022-08-13 10:41:57 +0700

    model updated

--- backend/src/Models/Drivers.js
+++ backend/src/Models/Drivers.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const driverSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    name: { type: String, required: true },
-    phone: { type: Number, required: true },
-    idCard: { type: string, required: false },
-    licence: { type: string, required: false }
+  _id: mongoose.Schema.Types.ObjectId,
+  name: { type: String, required: true },
+  phone: { type: Number, required: true },
+  idCard: { type: String, required: false },
+  licence: { type: String, required: false },
 });
 
 module.exports = mongoose.model('Driver', driverSchema);
--- backend/src/Models/Trucks.js
+++ backend/src/Models/Trucks.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
 const truckSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    licenceNumber: { type: String, required: true },
-    licenceType: { type: String, required: true },
-    truckType: { type: String, required: true },
-    productionYear: { type: Number, required: false },
-    stnk: { type: string, required: false },
-    kir: { type: string, required: false }
+  _id: mongoose.Schema.Types.ObjectId,
+  licenceNumber: { type: String, required: true },
+  licenceType: { type: String, required: true },
+  truckType: { type: String, required: true },
+  productionYear: { type: Number, required: false },
+  stnk: { type: String, required: false },
+  kir: { type: String, required: false },
 });
 
 module.exports = mongoose.model('Truck', truckSchema);
