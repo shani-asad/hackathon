63ab8803cbd1c3a476bcd6fd5de38a566d6fa852
shani <shani@medigo.id>
2022-08-13 14:13:35 +0700

    fix conflict

--- backend/src/Exceptions/Exceptions.js
+++ backend/src/Exceptions/Exceptions.js
@@ -21,10 +21,4 @@ const serverError = (res, message) => {
 
 };
 
-
-<<<<<<< HEAD:backend/src/Exceptions/NotFoundError.js
-module.exports = NotFoundError;
-module.exports = BadRequest;
-=======
-module.exports = {NotFoundError, BadRequest, serverError,};
->>>>>>> ca800c83587e97d3129779a91e2b61d4c0e0c18a:backend/src/Exceptions/Exceptions.js
+module.exports = {NotFoundError, BadRequest, serverError};
