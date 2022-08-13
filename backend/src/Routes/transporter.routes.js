4382fb96608d4196295f7cd62b30efd2b1dacd28
agungardiyanta <agungardiyanta@gmail.com>
2022-08-13 10:45:22 +0700

    add transporter routes

--- backend/src/Routes/transporter.routes.js
+++ backend/src/Routes/transporter.routes.js
@@ -7,11 +7,17 @@ const router = express.Router();
 router.post('/login', transporterControllers.transLogin);
 //ticket2
 router.get('/trucks', transporterControllers.transGetTruck);
-router.get('/trucks/details', transporterControllers.transGetTruckDetails);
+router.get('/trucks/details', transporterControllers.GetTruckDetails);
 //ticket3
 router.post('addTrucks', transporterControllers.transAddTruck);
-router.update('editTruck', transporterControllers.transUpdateTruck);
+router.put('editTruck', transporterControllers.transUpdateTruck);
 //ticket4
+router.get('drivers',transporterControllers.transGetDriver);
+router.get('/drivers/details', transporterControllers.transGetDriverDetails);
+//ticket5
+router.post('/addDriver',transporterControllers.transAddDriver);
+router.put('/editDriver',transporterControllers.transUpdateDriver);
+
 
 
 module.exports = router;
