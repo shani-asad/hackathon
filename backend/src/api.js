d9b83a511076995ad77c27d04e65c8f0ab8fe130
Gerry Steven <sgerrys93@gmail.com>
2022-08-13 10:49:26 +0700

    api update

--- backend/src/api.js
+++ backend/src/api.js
@@ -7,5 +7,9 @@ const notesRouter = require('./Routes/notes.routes');
 
 router.use('/notes', notesRouter);
 
+const transporterRouter = require('./Routes/transporter.routes')
+
+router.use('/transporter', transporterRouter);
+
 // RETURN ROUTER AS MODULE
 module.exports = router;
