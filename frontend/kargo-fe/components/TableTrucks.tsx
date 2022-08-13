c539ca510beb7d479a048ed70de5d353b987021f
ojimanda <yozimanda11@gmail.com>
2022-08-13 12:49:52 +0700

    edit dropdown truck type

--- frontend/kargo-fe/components/TableTrucks.tsx
+++ frontend/kargo-fe/components/TableTrucks.tsx
@@ -10,16 +10,20 @@ export default function TableTrucks() {
     <div>
       <div className="p-5 flex flex-row justify-between">
         <div>
-          <select
-            id="underline_select"
-            className="block py-2.5 px-0 text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
-          >
-            <option selected>Choose a country</option>
-            <option value="US">United States</option>
-            <option value="CA">Canada</option>
-            <option value="FR">France</option>
-            <option value="DE">Germany</option>
-          </select>
+          <div className="relative inline-flex">
+            <svg className="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232">
+              <path
+                d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z"
+                fill="#648299"
+              />
+            </svg>
+            <select className="border border-gray-300 rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white hover:border-gray-400 focus:outline-none appearance-none">
+              <option>Truck Type</option>
+              <option value="tronton">Tronton</option>
+              <option value="container  ">Container</option>
+
+            </select>
+          </div>
         </div>
 
         <div>
