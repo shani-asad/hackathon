78c34b2554a203d571ee29df8509e37d76e1b428
jovan <jovanus@yahoo.co.id>
2022-08-13 10:36:37 +0700

    init shipper page

---
+++ frontend/kargo-fe/pages/shipper/index.tsx
@@ -0,0 +1,17 @@
+import {NextPage} from "next";
+import Head from "next/head";
+
+const Shipper: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Shipper Page</title>
+            </Head>
+            <main>
+                <h1>This is Shipper Page!</h1>
+            </main>
+        </>
+    )
+}
+
+export default Shipper
