d975bde72928a4ee1b491c63a014ab1e5ba262e4
thomasfm <13519099@std.stei.itb.ac.id>
2022-08-13 12:21:05 +0700

    chore : add table head component

---
+++ frontend/kargo-fe/components/drivers/TableHead.tsx
@@ -0,0 +1,85 @@
+import {
+  TableHead,
+  TableRow,
+  TableCell,
+  TableSortLabel,
+  Box,
+} from "@mui/material";
+import { DriverTable, TableProps } from "../../types/driver";
+import { visuallyHidden } from "@mui/utils";
+
+interface HeadCell {
+  disablePadding: boolean;
+  id: keyof DriverTable;
+  label: string;
+  numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: "name",
+    numeric: false,
+    disablePadding: true,
+    label: "Driver Name",
+  },
+  {
+    id: "phoneNumber",
+    numeric: false,
+    disablePadding: false,
+    label: "Phone Number",
+  },
+  {
+    id: "createdAt",
+    numeric: false,
+    disablePadding: false,
+    label: "Created At",
+  },
+  {
+    id: "status",
+    numeric: false,
+    disablePadding: false,
+    label: "Status",
+  },
+  {
+    id: "dropdown",
+    numeric: false,
+    disablePadding: false,
+    label: "",
+  },
+];
+
+export function HeadTable(props: TableProps) {
+  const { order, orderBy, rowCount, onRequestSort } = props;
+  const createSortHandler =
+    (property: keyof DriverTable) => (event: React.MouseEvent<unknown>) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? "right" : "left"}
+            padding={headCell.disablePadding ? "none" : "normal"}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : "asc"}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === "desc" ? "sorted descending" : "sorted ascending"}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
